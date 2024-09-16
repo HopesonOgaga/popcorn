9da3c2ce38724fb37571e5c22dd7bb5133c8910b
hopeson <hopesonogaga@gmail.com>
2024-09-16 03:11:38 +0100

    star

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5}></StarRating>
     <StarRating maxRating={10}></StarRating>
   </React.StrictMode>
 );
